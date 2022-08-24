348113ebf57d24a1bfc996e5158d433ea2f43ff5
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-08-24 20:06:22 -0300

    exercicios parte 2 completo

--- exercicios/back-end/bloco-22-intruducao-ao-node/dia-4-express-middlewares/exercicios_dia/src/app.js
+++ exercicios/back-end/bloco-22-intruducao-ao-node/dia-4-express-middlewares/exercicios_dia/src/app.js
@@ -5,6 +5,8 @@ const validationDesc = require("./middlewares/middlewareDesc");
 const validationCreated = require("./middlewares/middlewareCreated");
 const validationRating = require("./middlewares/middlewareRating");
 const validationDifficulty = require("./middlewares/middlewareDifficulty");
+const auth = require("./middlewares/auth");
+const generateToken = require("./files/generateToken");
 
 const app = express();
 app.use(express.json());
@@ -17,9 +19,22 @@ app.post(
   validationCreated,
   validationRating,
   validationDifficulty,
+  auth,
   (req, res) => {
     res.status(201).json({ message: "Atividade cadastrada com sucesso!" });
   }
 );
+
+app.post('/signup', (_req, res) => {
+  const { email, password, firstName, phone } = req.body;
+
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+
+  const token = generateToken();
+
+  return res.status(200).json({ token });
+});
 
 module.exports = app;
