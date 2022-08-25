0ff7df688a93ed32a91e484543c682249d09d296
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-08-25 18:13:26 -0300

    conteudo do dia

---
+++ exercicios/back-end/bloco-22-intruducao-ao-node/dia-5-express-e-mysql/exercicio-do-dia/trybecash-api/src/server.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+require('dotenv').config();
+const connection = require('./db/connection');
+
+const port = 3001;
+
+app.listen(port, async () => {
+    console.log(`API TrybeCash está sendo executada na porta ${port}`);
+    console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
+  });
